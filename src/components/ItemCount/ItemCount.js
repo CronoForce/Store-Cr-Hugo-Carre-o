05f47fc81f7e77e4ad9f38542ca7f72244b3045c
CronoForce <hugo.karreno@gmail.com>
2021-04-12 20:33:48 -0500

    Hugo Carreño Desafio 6

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -2,32 +2,16 @@ import React from "react";
 import "./ItemCount.css";
 
 function ItemCount() {
-  var cant = 0;
-  const Add_Prodcut = () => {
-    var text_cant = document.querySelector("#car-cant");
-    cant = cant + 1;
-
-    text_cant.innerHTML = cant;
-  };
-
-  const Less_Product = () => {
-    var text_cant = document.querySelector("#car-cant");
-    cant = cant - 1;
-
-    text_cant.innerHTML = cant
-    
-  };
-
-  return (
+    return (
     <div className="itemCount">
       <div className="itemCount-Container">
-        <a onClick={Add_Prodcut} className="btn-car-add">
+        <a className="btn-car-add">
           +
         </a>
         <p className="car-cant" id="car-cant">
           0
         </p>
-        <a onClick={Less_Product} className="btn-car-less">-</a>
+        <a className="btn-car-less">-</a>
       </div>
       <a href="" className="btn-add-car">
         Añadir al carrito
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,20 +1,11 @@
 import React from "react";
 import "./ItmeListContainer.css";
-import ItemCount from '../ItemCount/ItemCount'
+import Item from "../Items/item";
 
 function Saludo() {
     return (
         <div className='listcontainer'>
-            <div className='container-item'>
-                <div className='container-item__greeting'>
-                    <img src="https://i.ibb.co/BP0W354/audifonos.png" alt="audifonos" />
-                    <div className='container-item__greeting__text'>
-                        <p className='price'>USD $200</p>
-                        <p className='description'>Lorem ipsum dolor sit.</p>
-                        <ItemCount />
-                    </div>
-                </div>
-            </div>
+            <Item />
         </div>
     )
 }
