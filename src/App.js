5f8898c71c78adb6c16ba62a2d7dbd5d364bc6e2
CronoForce <hugo.karreno@gmail.com>
2021-04-21 15:40:24 -0500

    Hugo Carreño - Desafío: Primera Entrega del Proyecto Final

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Navbar from "./components/NavBar/NavBar";
 import "./App.css";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import Home from "./components/Home/Home";
-import Categorias from './components/Categorias/Categorias';
-import Login from './components/Login/Login';
-import Contacto from './components/Contacto/Contacto';
+import Categorias from "./components/Categorias/Categorias";
+import Login from "./components/Login/Login";
+import Contacto from "./components/Contacto/Contacto";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -13,11 +13,13 @@ function App() {
     <Router>
       <Fragment>
         <Navbar />
-        <Route path="/" exact component={Home} />
-        <Route path="/categoria" component={Categorias} />
-        <Route path="/lomasvendido" component={ItemListContainer} />
-        <Route path="/contacto" component={Contacto} />
-        <Route path="/login" component={Login} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/categoria" component={Categorias} />
+          <Route path="/lomasvendido" component={ItemListContainer} />
+          <Route path="/contacto" component={Contacto} />
+          <Route path="/login" component={Login} />
+        </Switch>
       </Fragment>
     </Router>
   );
